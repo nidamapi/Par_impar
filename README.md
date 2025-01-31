568f1c1f819caaa455032f789e0eeecde0aba1f9
nidamapi <nicollemacias@colegioguanenta.edu.co>
2025-01-31 10:13:08 -0500

    actualizacion

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Par_impar
 Programa para verificar si un numero es par o impar
+
+Construir un programa que lea un numero entero y que determine si se trata de un numero par
---
+++ par_impar.py
@@ -0,0 +1,7 @@
+num = int(input("Ingrese un numero para determinar si es par o impar "))
+
+if num % 2 == 0:
+    print(f"El nùmero {num} es par")
+else:
+    print(f"El nùmero {num} es impar")
+    
